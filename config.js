0d5736c6db20c7141d2dfd31084e4c3766f91c05
Truong Duc Minh <truongducminh@outlook.com>
2017-03-28 15:54:02 +0700

    fix: product image url

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const DIRNAME = __dirname;
-const DOMAIN_NAME = 'https://bs-auction.herokuapp.com/';
+const DOMAIN_NAME = 'https://bs-auction.herokuapp.com';
 const PORT = process.env.PORT || 3000;
 module.exports = Object.assign({ PORT,DOMAIN_NAME,DIRNAME });
